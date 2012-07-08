7bdee22e78af0df4823b150859dab315b5ae77e8
Shad <shad.downey@gmail.com>
2012-07-08 13:06:55 -0700

    undo

--- load.php
+++ load.php
@@ -1,6 +1,4 @@
 <?php
-echo __FILE__ . ':' . __LINE__ . "\n";exit;
-
 require_once(__DIR__ . DIRECTORY_SEPARATOR . 'constants.php');//All should be optional
 require_once(RADCANON_DIR . 'functions.php');
 spl_autoload_register('radcanon_autoloader');
