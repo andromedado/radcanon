d79d47a4717ddea9abaf1d635cbf2cf1f1b381b1
Shad <shad.downey@gmail.com>
2012-07-11 12:07:05 -0700

    Table DDL added

--- classes/Model/IpLog.php
+++ classes/Model/IpLog.php
@@ -6,7 +6,7 @@ class ModelIpLog extends Model {
 	protected $user_agent = '';
 	protected $hits = 0;
 	protected $last_hit;
-	protected $reputation;
+	protected $reputation = 0;
 	
 	protected $dbFields = array(
 		'ip',
@@ -25,4 +25,15 @@ class ModelIpLog extends Model {
 	protected static $AllData = array();
 }
 
-?>
+/*
+CREATE TABLE `ip_log` (
+  `ip` bigint(20) NOT NULL,
+  `pip` varchar(60) NOT NULL DEFAULT '',
+  `user_agent` varchar(255) NOT NULL DEFAULT '',
+  `hits` bigint(20) NOT NULL,
+  `last_hit` int(11) NOT NULL,
+  `reputation` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ip`),
+  KEY `user_agent` (`user_agent`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+ */
