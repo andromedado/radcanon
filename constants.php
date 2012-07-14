9d0e7c70e6bcba7e924f495814cd7660504162c3
Shad <shad.downey@gmail.com>
2012-07-13 18:29:55 -0700

    Added RadCanon Cache Dir and Constants

--- constants.php
+++ constants.php
@@ -1,7 +1,9 @@
 <?php
 if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-if (!defined('RADCANON_DIR')) define('RADCANON_DIR', __DIR__ . DS);
 if (!defined('LIB_DIR')) define('LIB_DIR', dirname(__DIR__) . DS);
+if (!defined('RADCANON_DIR')) define('RADCANON_DIR', __DIR__ . DS);
+if (!defined('RADCANON_CACHE_DIR')) define('RADCANON_CACHE_DIR', RADCANON_DIR . 'cache' . DS);
+if (!defined('CACHE_DIR')) define('CACHE_DIR', RADCANON_CACHE_DIR);
 if (!defined('MAIL_FROM')) define('MAIL_FROM', 'Info <info@radcanon.com>');
 if (!defined('USE_HTACCESS')) define('USE_HTACCESS', true);
 if (!class_exists('Twig_Autoloader')) {
