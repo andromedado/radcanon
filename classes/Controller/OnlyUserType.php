fbe3a9a1aa6df2a099cab9281aa542e16c0c21bc
Shad <shad.downey@gmail.com>
2012-07-23 10:48:05 -0700

    User Restricted controllers now optionally throw reroute exceptions

--- classes/Controller/OnlyUserType.php
+++ classes/Controller/OnlyUserType.php
@@ -2,6 +2,7 @@
 
 abstract class ControllerOnlyUserType extends ControllerApp {
 	protected $UserType = 'AuthUser';
+	protected $reRouteTo = array();
 	protected $permittedMethods = array(
 		'notFound'
 	);
@@ -9,10 +10,18 @@ abstract class ControllerOnlyUserType extends ControllerApp {
 	public function prefilterInvocation (&$method, array &$arguments) {
 		parent::prefilterInvocation($method, $arguments);
 		if (!in_array($method, $this->permittedMethods) && !is_a($this->user, $this->UserType)) {
+			$this->throwReRoute();
 			if (DEBUG) {
 				$this->set('tried', array($method, $arguments));
 			}
 			$method = 'notFound';
+		}
+	}
+	
+	protected function throwReRoute()
+	{
+		if (!empty($this->reRouteTo)) {
+			throw new ExceptionReroute(FilterRoutes::buildUrl($this->reRouteTo, false, false));
 		}
 	}
 	
