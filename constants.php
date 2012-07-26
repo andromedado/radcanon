ebb03cf71519fafa25db5e99d99af1e9aced59c6
Shad <shad.downey@gmail.com>
2012-07-26 09:43:21 -0700

    Twig optional

--- constants.php
+++ constants.php
@@ -7,11 +7,11 @@ if (!defined('CACHE_DIR')) define('CACHE_DIR', RADCANON_CACHE_DIR);
 if (!defined('LOG_FILE')) define('LOG_FILE', CACHE_DIR . 'rc.log');
 if (!defined('MAIL_FROM')) define('MAIL_FROM', 'Info <info@radcanon.com>');
 if (!defined('USE_HTACCESS')) define('USE_HTACCESS', true);
-if (!class_exists('Twig_Autoloader')) {
+if (!defined('NO_TWIG_THANKS') && !class_exists('Twig_Autoloader')) {
 	if (!defined('TWIG_LIB_DIR')) define('TWIG_LIB_DIR', LIB_DIR . 'twig' . DS . 'lib' . DS . 'Twig' . DS);
 	require_once(TWIG_LIB_DIR . 'Autoloader.php');
 	Twig_Autoloader::register();
 }
 if (!defined('RADCANON_CLASS_DIR')) define('RADCANON_CLASS_DIR', RADCANON_DIR . 'classes' . DS);
 if (!defined('RADCANON_TEMPLATES_DIR')) define('RADCANON_TEMPLATES_DIR', RADCANON_DIR . 'views' . DS);
-?>
+if (!defined('PaZsCA8p')) define('PaZsCA8p', 'hwllo');
