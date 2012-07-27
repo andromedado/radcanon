9af01422fae187dca4e5e6d0ebd25b5a554d45ed
Shad <shad.downey@gmail.com>
2012-07-27 10:27:09 -0700

    backwards compatablity method "timeFormatSeconds"

--- classes/Utils/Number.php
+++ classes/Utils/Number.php
@@ -128,6 +128,27 @@ abstract class UtilsNumber {
 		return (int)preg_replace('/[^\d'.($allowNegative ? '-' : '').']+/','',$str);
 	}
 
-}
+	public static function timeFormatSeconds ($secs, $includeSeconds = false)
+	{
+		$hrs = floor($secs / 60 / 60);
+		if ($includeSeconds) {
+			$mins = floor(($secs % 3600) / 60);
+			$sec = ($secs % 3600) % 60;
+			$Ss = $sec == 1 ? '' : 's';
+			$sec = ' ' . $sec . ' second' . $Ss;
+		} else {
+			$mins = round(($secs % 3600) / 60);
+			$sec = '';
+		}
+		$Hs = $hrs == 1 ? '' : 's';
+		if ($hrs > 0) {
+			 $hrs = $hrs . ' hr' . $Hs . '. ';
+		} else {
+			$hrs = '';
+		}
+		$Ms = $mins == 1 ? '' : 's';
+		$mins = $mins . ' min' . $Ms . '.';
+		return $hrs . $mins . $sec;
+	}
 
-?>
+}
