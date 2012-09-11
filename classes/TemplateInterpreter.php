a188a5b03a2a1c9d08627435dfbee476e830ca2f
Shad <shad.downey@gmail.com>
2012-09-11 14:30:03 -0700

    update, template interpreter will always strip fields it knows about, even if it doesnt have anything to swap in

--- classes/TemplateInterpreter.php
+++ classes/TemplateInterpreter.php
@@ -5,17 +5,29 @@ class TemplateInterpreter
 	public $tplFields;
 	public $tplFieldDelimiter;
 	
+	/**
+	 * @param Array $tplFields
+	 * @param String $tplFieldDelimiter
+	 */
 	public function __construct(array $tplFields, $tplFieldDelimiter = '')
 	{
 		$this->tplFields = $tplFields;
 		$this->tplFieldDelimiter = $tplFieldDelimiter;
 	}
 	
-	public function renderWith($content, array $renderWith = array(), $finalWrapOpen = '', $finalWrapClose = '')
+	/**
+	 * With the content containing tpl fields, use the given tpl values to render
+	 * Optionally add a pre- and post-fix to all replacements
+	 * @param String $content
+	 * @param Array $renderWith tpl Values (field => value)
+	 * @param String $preFix
+	 * @param String $postFix
+	 */
+	public function renderWith($content, array $renderWith = array(), $preFix = '', $postFix = '')
 	{
-		foreach ($renderWith as $tplField => $value) {
-			if (!in_array($tplField, $this->tplFields)) continue;
-			$content = str_replace($this->tplFieldDelimiter . $tplField . $this->tplFieldDelimiter, $finalWrapOpen . $value . $finalWrapClose, $content);
+		foreach ($this->tplFields as $tplField) {
+			$repWith = array_key_exists($tplField, $renderWith) ? $renderWith[$tplField] : '';
+			$content = str_replace($this->tplFieldDelimiter . $tplField . $this->tplFieldDelimiter, $preFix . $repWith . $postFix, $content);
 		}
 		return $content;
 	}
