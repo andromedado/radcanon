672885db42a4389977c9fab41cd6cba20fe2e6e2
Shad <shad.downey@gmail.com>
2012-09-18 10:52:36 -0700

    `updateCacheValue` can now update ALL cached values; new method `updateAll` to hit all rows in a table

--- classes/Model/Model.php
+++ classes/Model/Model.php
@@ -506,6 +506,12 @@ abstract class Model implements Iterator
 	}
 	
 	protected static function updateCacheValue($id, $var, $val) {
+		if (is_null($id)) {
+			foreach (static::$AllData as &$data) {
+				$data[$var] = $val;
+			}
+			return true;
+		}
 		if (!isset(static::$AllData[$id]) || !is_array(static::$AllData[$id])) static::$AllData[$id] = array();
 		return static::$AllData[$id][$var] = $val;
 	}
@@ -624,6 +630,28 @@ abstract class Model implements Iterator
 		}
 		$this->updateFollowUp(array_keys($varsToVals));
 		return true;
+	}
+	
+	public static function updateAll(array $varsToVals)
+	{
+		if (!empty($varsToVals)) {
+			$sets = $comma = '';
+			$args = array_values($varsToVals);
+			foreach ($varsToVals as $field => $value) {
+				$sets .= $comma . DBCFactory::quote($field) . " = ?";
+				$comma = ', ';
+			}
+			$sql = "UPDATE " . DBCFactory::quote(static::$Table) . " " .
+			"SET " . $sets;
+			$stmt = DBCFactory::wPDO()->prepare($sql);
+			if (!$stmt) {
+				throw new ExceptionBase(DBCFactory::wPDO()->errorInfo(), '1');
+			}
+			if (!$stmt->execute($args)) {
+				throw new ExceptionPDO($stmt, $sql, '1');
+			}
+			static::updateCacheValues(null, $varsToVals);
+		}
 	}
 	
 	/**
