f642c2949861b9a59a6f84f88a61ed7cd64aaaaa
Shad <shad.downey@gmail.com>
2012-09-21 12:55:38 -0700

    new global function vdumpF that doesnt care about DEBUG mode

--- functions.php
+++ functions.php
@@ -30,17 +30,21 @@ function radcanon_autoloader ($classname) {
 
 function vdump () {
 	if (defined('DEBUG') && DEBUG) {
-		$i = 1;
-		while (ob_get_level() && $i < 5) {
-			ob_end_flush();
-			$i++;
-		}
 		$args = func_get_args();
-		echo '<html><head></head><body><pre>';
-		call_user_func_array('var_dump', $args);
-		echo '</pre></body></html>';
-		exit;
+		call_user_func_array('vdumpF', $args);
 	}
 }
 
-?>
+function vdumpF () {
+	$i = 1;
+	while (ob_get_level() && $i < 5) {
+		ob_end_flush();
+		$i++;
+	}
+	$args = func_get_args();
+	echo '<html><head></head><body><pre>';
+	call_user_func_array('var_dump', $args);
+	echo '</pre></body></html>';
+	exit;
+}
+
