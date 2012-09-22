60334ed2b3dac6db36ba7091141400372b1423f9
Shad <shad.downey@gmail.com>
2012-09-22 14:31:50 -0700

    bugfix

--- classes/Filter/Admin.php
+++ classes/Filter/Admin.php
@@ -1,11 +1,12 @@
 <?php
 
-class FilterAdmin implements Filter {
+class FilterAdmin implements Filter
+{
 	
 	public function filter(Request $Request, Response $Response, User $User) {
 		if (is_a($User, 'Admin') && !$Request->isPost()) {
 			if ($User->requiresPasswordChange()) {
-				$Request->setURI(FilterRoutes::buildUrl(array('Admin', 'changePassword', 'must')));
+				$Request->setURI(FilterRoutes::buildUrl(array('Admin', 'changePassword', 'must'), true));
 			}
 		}
 	}
