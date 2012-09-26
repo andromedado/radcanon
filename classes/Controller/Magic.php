51d280ff42f78d495bc5540f59be8ade5d2f7677
Shad <shad.downey@gmail.com>
2012-09-26 15:02:21 -0700

    index magic

--- classes/Controller/Magic.php
+++ classes/Controller/Magic.php
@@ -78,6 +78,9 @@ class ControllerMagic extends ControllerApp
 		$args = func_get_args();
 		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
 		if (!$permitted) return $return;
+		if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'index.html.twig')) {
+			$this->response->template = 'Model' . DS . 'index.html.twig';
+		}
 		return $this->_index();
 	}
 	
