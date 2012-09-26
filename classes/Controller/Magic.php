acf97a04e3ba86886db06569fd08af3848b0947d
Shad <shad.downey@gmail.com>
2012-09-26 11:02:50 -0700

    ControllerMagic is much more magical now

--- classes/Controller/Magic.php
+++ classes/Controller/Magic.php
@@ -2,29 +2,76 @@
 
 class ControllerMagic extends ControllerApp
 {
+	protected $GateKeeperMethods = array(
+	);
+	
+	protected function mayProceed($invoked, array $args)
+	{
+		$permitted = $return = true;
+		foreach ($this->GateKeeperMethods as $method => $methodOnFailure) {
+			if (call_user_func(array($this, $method), $invoked, $args) !== true) {
+				$permitted = false;
+				if (is_null($methodOnFailure)) {
+					$return = $this->notFound();
+				} else {
+					$return = call_user_func(array($this, $methodOnFailure), $invoked, $args);
+				}
+				break;
+			}
+		}
+		return array($permitted, $return);
+	}
+	
+	protected function determineDestination($invoked, array $args)
+	{
+		switch ($invoked) {
+			case "update":
+				$id = isset($args[0]) ? $args[0] : 0;
+				$dest = array($this->baseName, 'review', $id);
+				break;
+			default:
+				$dest = array($this->baseName);
+		}
+		return $dest;
+	}
 	
 	public function create()
 	{
-		return $this->_create(array('destination' => array($this->baseName)));
+		$args = func_get_args();
+		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+		if (!$permitted) return $return;
+		return $this->_create(array('destination' => $this->determineDestination(__FUNCTION__, $args)));
 	}
 	
 	public function review($id = null)
 	{
+		$args = func_get_args();
+		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+		if (!$permitted) return $return;
 		return $this->_review($id);
 	}
 	
 	public function update($id = null)
 	{
-		return $this->_update($id, array('destination' => array($this->baseName, 'review', $id)));
+		$args = func_get_args();
+		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+		if (!$permitted) return $return;
+		return $this->_update($id, array('destination' => $this->determineDestination(__FUNCTION__, $args)));
 	}
 	
 	public function delete($id = null)
 	{
-		return $this->_delete($id, array('destination' => array($this->baseName)));
+		$args = func_get_args();
+		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+		if (!$permitted) return $return;
+		return $this->_delete($id, array('destination' => $this->determineDestination(__FUNCTION__, $args)));
 	}
 	
 	public function index()
 	{
+		$args = func_get_args();
+		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+		if (!$permitted) return $return;
 		return $this->_index();
 	}
 	
