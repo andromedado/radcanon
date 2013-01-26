59d85780877ecce09a35e566d194996e7f16a1f7
Shad <shad.downey@gmail.com>
2013-01-26 12:59:22 -0500

    more fine-grained defaulting for magic controllers; and now theres a default ajax->html representation of the `index` page

--- classes/Controller/Magic.php
+++ classes/Controller/Magic.php
@@ -2,121 +2,148 @@
 
 class ControllerMagic extends ControllerApp
 {
-	protected $GateKeeperMethods = array(
-	);
-	
-	protected function isAdmin()
-	{
-		return is_a($this->user, 'Admin');
-	}
-	
-	protected function isAuthUser()
-	{
-		return is_a($this->user, 'AuthUser');
-	}
-	
-	protected function mayProceed($invoked, array $args)
-	{
-		$permitted = $return = true;
-		foreach ($this->GateKeeperMethods as $method => $methodOnFailure) {
-			if (!method_exists($this, $method)) {
-				throw new ExceptionBase('Invalid Method offered by `GetKeeperMethods`: ' . $method);
-			}
-			if (call_user_func(array($this, $method), $invoked, $args) !== true) {
-				$permitted = false;
-				if (is_null($methodOnFailure)) {
-					$return = $this->notFound();
-				} else {
-					$return = call_user_func(array($this, $methodOnFailure), $invoked, $args);
-				}
-				break;
-			}
-		}
-		return array($permitted, $return);
-	}
-	
-	protected function determineDestination($invoked, array $args)
-	{
-		if ($this->request->post('_bounceBack', null) !== null) {
-			return $this->request->post('_bounceBack');
-		}
-		switch ($invoked) {
-			case "update":
-				$id = isset($args[0]) ? $args[0] : 0;
-				$dest = array($this->baseName, 'review', $id);
-				break;
-			default:
-				$dest = array($this->baseName);
-		}
-		return $dest;
-	}
-	
-	protected function determineSettings($invoked, array $args)
-	{
-		$this->set('_uri', $args);
-		$settings = array();
-		switch($invoked) {
-			case 'delete':
-			case 'update':
-			case 'create':
-				$settings['destination'] = $this->determineDestination($invoked, $args);
-				break;
-			
-		}
-		return $settings;
-	}
-	
-	public function create()
-	{
-		$args = func_get_args();
-		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
-		if (!$permitted) return $return;
-		if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'create.html.twig')) {
-			$this->response->template = 'Model' . DS . 'create.html.twig';
-		}
-		return $this->_create($this->determineSettings(__FUNCTION__, $args));
-	}
-	
-	public function review($id = null)
-	{
-		$args = func_get_args();
-		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
-		if (!$permitted) return $return;
-		if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'review.html.twig')) {
-			$this->response->template = 'Model' . DS . 'review.html.twig';
-		}
-		return $this->_review($id, $this->determineSettings(__FUNCTION__, $args));
-	}
-	
-	public function update($id = null)
-	{
-		$args = func_get_args();
-		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
-		if (!$permitted) return $return;
-		if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'update.html.twig')) {
-			$this->response->template = 'Model' . DS . 'update.html.twig';
-		}
-		return $this->_update($id, $this->determineSettings(__FUNCTION__, $args));
-	}
-	
-	public function delete($id = null)
-	{
-		$args = func_get_args();
-		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
-		if (!$permitted) return $return;
-		return $this->_delete($id, $this->determineSettings(__FUNCTION__, $args));
-	}
-	
-	public function index()
-	{
-		$args = func_get_args();
-		list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
-		if (!$permitted) return $return;
-		if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'index.html.twig')) {
-			$this->response->template = 'Model' . DS . 'index.html.twig';
-		}
-		return $this->_index($this->determineSettings(__FUNCTION__, $args));
-	}
-	
+    protected $GateKeeperMethods = array(
+    );
+
+    protected function isAdmin()
+    {
+        return is_a($this->user, 'Admin');
+    }
+
+    protected function isAuthUser()
+    {
+        return is_a($this->user, 'AuthUser');
+    }
+
+    protected function mayProceed($invoked, array $args)
+    {
+        $permitted = $return = true;
+        foreach ($this->GateKeeperMethods as $method => $methodOnFailure) {
+            if (!method_exists($this, $method)) {
+                throw new ExceptionBase('Invalid Method offered by `GetKeeperMethods`: ' . $method);
+            }
+            if (call_user_func(array($this, $method), $invoked, $args) !== true) {
+                $permitted = false;
+                if (is_null($methodOnFailure)) {
+                    $return = $this->notFound();
+                } else {
+                    $return = call_user_func(array($this, $methodOnFailure), $invoked, $args);
+                }
+                break;
+            }
+        }
+        return array($permitted, $return);
+    }
+
+    protected function determineDestination($invoked, array $args)
+    {
+        if ($this->request->post('_bounceBack', null) !== null) {
+            return $this->request->post('_bounceBack');
+        }
+        switch ($invoked) {
+            case "update":
+                $id = isset($args[0]) ? $args[0] : 0;
+                $dest = array($this->baseName, 'review', $id);
+                break;
+            default:
+                $dest = array($this->baseName);
+        }
+        return $dest;
+    }
+
+    protected function determineSettings($invoked, array $args)
+    {
+        $this->set('_uri', $args);
+        $settings = array();
+        switch($invoked) {
+            case 'delete':
+            case 'update':
+            case 'create':
+                $settings['destination'] = $this->determineDestination($invoked, $args);
+                break;
+            
+        }
+        return $settings;
+    }
+
+    public function create()
+    {
+        $args = func_get_args();
+        list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+        if (!$permitted) return $return;
+        if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'create.html.twig')) {
+            $this->response->template = 'Model' . DS . 'create.html.twig';
+        }
+        return $this->_create($this->determineSettings(__FUNCTION__, $args));
+    }
+
+    public function review($id = null)
+    {
+        $args = func_get_args();
+        list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+        if (!$permitted) return $return;
+        if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'review.html.twig')) {
+            $this->response->template = 'Model' . DS . 'review.html.twig';
+        }
+        return $this->_review($id, $this->determineSettings(__FUNCTION__, $args));
+    }
+
+    public function update($id = null)
+    {
+        $args = func_get_args();
+        list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+        if (!$permitted) return $return;
+        if (!file_exists(APP_TEMPLATES_DIR . $this->getTemplateDir() . DS . 'update.html.twig')) {
+            $this->response->template = 'Model' . DS . 'update.html.twig';
+        }
+        return $this->_update($id, $this->determineSettings(__FUNCTION__, $args));
+    }
+
+    public function delete($id = null)
+    {
+        $args = func_get_args();
+        list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+        if (!$permitted) return $return;
+        return $this->_delete($id, $this->determineSettings(__FUNCTION__, $args));
+    }
+
+    public function index()
+    {
+        $args = func_get_args();
+        list($permitted, $return) = $this->mayProceed(__FUNCTION__, $args);
+        if (!$permitted) return $return;
+        if ($this->request->isAjax()) {
+            try {
+                $this->defaultTemplate = $this->response->template = $this->getTemplate('ajIndex');
+                $this->response->type = Response::TYPE_TEMPLATE_IN_JSON;
+            } catch (ExceptionBase $e) {}
+        }
+        if (!$this->response->templateExists($this->defaultTemplate)) {
+            $this->response->template = 'Model' . DS . 'index.html.twig';
+        }
+        $this->set('listTemplate', $this->getTemplate('renderList'));
+        $this->set('liTemplate', $this->getTemplate('renderAsLi'));
+        return $this->_index($this->determineSettings(__FUNCTION__, $args));
+    }
+
+    protected function getTemplate($templateName)
+    {
+        $dirsToTry = array(
+            $this->getTemplateDir() . DS,
+            'Model' . DS,
+            '',
+        );
+        foreach ($dirsToTry as $dir) {
+            $t = $dir . $templateName . '.html.twig';
+            if ($exists = $this->response->templateExists($t)) {
+                break;
+            }
+        }
+        if (!$exists) {
+            throw new ExceptionBase('Unable to find template file for "' . $templateName . '"');
+        }
+        return $t;
+    }
+
 }
 
