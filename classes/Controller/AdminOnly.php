c3e46d27096a8826c5b578b1b8de970a8c801488
Shad Downey <shad@1stdibs.com>
2013-06-08 13:02:55 -0400

    admins can generally do it

--- classes/Controller/AdminOnly.php
+++ classes/Controller/AdminOnly.php
@@ -1,6 +1,12 @@
 <?php
 
 class ControllerAdminOnly extends ControllerOnlyUserType {
-	protected $UserType = 'Admin';
+    protected $UserType = 'Admin';
+
+    public function editAttr($id = null, $attr = null)
+    {
+        return parent::_editAttr($id, $attr);
+    }
+
 }
 
