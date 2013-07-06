cce99358e655d005c61aa2645ff1c76a47622284
Shad Downey <shad@1stdibs.com>
2013-07-06 14:13:31 -0400

    accept multi-uploads

--- classes/File/MultImage.php
+++ classes/File/MultImage.php
@@ -2,8 +2,18 @@
 
 class FileMultImage extends FileOfModel
 {
-	public $validationPreg = UtilsString::IMAGE_FILENAME_REGEXP;
-	
-	
+    public $validationPreg = UtilsString::IMAGE_FILENAME_REGEXP;
+
+    public function acceptUpload()
+    {
+        $this->checkForUploadErrors();
+        foreach ($_FILES[$this->name]['name'] as $index => $name) {
+            $dest = $this->getBaseDir() . UtilsString::urlSafe($name, true);
+            if (!move_uploaded_file($_FILES[$this->name]['tmp_name'][$index], $dest)) {
+                throw new ExceptionBase('Couldnt move from ' . $_FILES[$this->name]['tmp_name'] . ' to ' . $dest);
+            }
+        }
+    }
+
 }
 
