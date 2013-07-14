9e612a1cb4a8d5550f9d8f41d51e6799d2700c4d
Shad Downey <shad@1stdibs.com>
2013-07-14 11:39:12 -0400

    more robust multi-image handling

--- classes/File/MultImage.php
+++ classes/File/MultImage.php
@@ -1,6 +1,6 @@
 <?php
 
-class FileMultImage extends FileOfModel
+class FileMultImage extends FileMultiple
 {
     public $validationPreg = UtilsString::IMAGE_FILENAME_REGEXP;
 
@@ -13,6 +13,35 @@ class FileMultImage extends FileOfModel
                 throw new ExceptionBase('Couldnt move from ' . $_FILES[$this->name]['tmp_name'] . ' to ' . $dest);
             }
         }
+    }
+
+    public function getSrcsToImagesAtWidth ($width) {
+        $paths = $this->getPathsToImagesAtWidth($width);
+        $srcs = array();
+        foreach ($paths as $path) {
+            $srcs[] = APP_SUB_DIR . str_replace(SERVER_PREFIX, '', $path);
+        }
+        return $srcs;
+    }
+
+    public function getPathsToImagesAtWidth($width) {
+        $width = abs((int)$width);
+        if ($width < 1) throw new ExceptionBase('invalid width');
+        $rd = $this->getResizeDir();
+        $fileNames = $this->getFilenames();
+        $paths = array();
+        foreach ($fileNames as $fileName) {
+            $nameParts = explode('.', $fileName);
+            $ext = array_pop($nameParts);
+            $nameParts[] = $width;
+            $nameParts[] = $ext;
+            $requestedName = implode('.', $nameParts);
+            if (!file_exists($rd . $requestedName)) {
+                UtilsImage::resizeImage($this->getBaseDir() . $fileName, $rd . $requestedName, $width);
+            }
+            $paths[] = $rd . $requestedName;
+        }
+        return $paths;
     }
 
 }
