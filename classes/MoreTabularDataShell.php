0a8a7410750bd4f0f1842080e2acf320d7c59049
Shad Downey <shad@1stdibs.com>
2013-12-22 00:55:29 -0500

    An abstract implementation for a tabularData wrapper!

---
+++ classes/MoreTabularDataShell.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Class MoreTabularDataShell
+ * A convenience class for when you've gone
+ * to all the trouble of implementing `ExposesTabularData`
+ * and now you want to expose a new set of columns
+ * Just re-implement `getColumns` and you're good to go!
+ */
+abstract class MoreTabularDataShell
+    implements ExposesTabularData
+{
+    /** @var \ExposesTabularData $wrapped */
+    protected $wrapped;
+
+    public function __construct(ExposesTabularData $wrapped)
+    {
+        $this->wrapped = $wrapped;
+    }
+
+    public function getRowAttributes()
+    {
+        return $this->wrapped->getRowAttributes();
+    }
+
+    public function getCellValueForColumn($column, $formatted = true)
+    {
+        return $this->wrapped->getCellValueForColumn($column, $formatted);
+    }
+
+    public function getCellAttributesForColumn($column)
+    {
+        return $this->wrapped->getCellAttributesForColumn($column);
+    }
+
+    public function hasTFoot()
+    {
+        return $this->wrapped->hasTFoot();
+    }
+
+    public function getTFootAttributes()
+    {
+        return $this->wrapped->getTFootAttributes();
+    }
+
+    public function getBaseTFootCellValueForColumn($column)
+    {
+        return $this->wrapped->getBaseTFootCellValueForColumn($column);
+    }
+
+    public function mutateTFootCellValueForColumn(&$value, $column)
+    {
+        return $this->wrapped->mutateTFootCellValueForColumn($value, $column);
+    }
+
+    public function finalTFootCellMutate(&$cell, $column, $formatted = true)
+    {
+        return $this->wrapped->finalTFootCellMutate($cell, $column, $formatted);
+    }
+
+    public static function wrapThem(array $them)
+    {
+        $wrapped = array();
+        foreach ($them as $it) {
+            $class = get_called_class();
+            $wrapped[] = new $class($it);
+        }
+        return $wrapped;
+    }
+
+}
+
